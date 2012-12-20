5aab66eadfdd5ce50d454e2f546700bc8dfb5350
ricardogarcia <ricardogarfe@gmail.com>
2012-12-20 22:13:23 +0000

    Puppet Redmine plugin installer.
        * Install WYSIWYG plugin for Redmine.

---
+++ puppet_plugins_redmine/README.markdown
@@ -0,0 +1,7 @@
+# redmine plugin installer #
+
+This is the redmine plugin installer module.
+
+Plugin list:
+
+	* Redmine wysiwyg plugin.
---
+++ puppet_plugins_redmine/tests/init.pp
@@ -0,0 +1,5 @@
+# Test class for wysiwyg intaller.
+
+class { "puppet_plugins_redmine::wysiwyg_textile_plugin":
+  installFolder => "/opt",
+}
