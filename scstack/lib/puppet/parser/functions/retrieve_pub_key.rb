8241bb5d1d7c01146a1e5192d67fdfdbaeeca5e7
ricardogarcia <ricardogarfe@gmail.com>
2013-03-13 12:11:23 +0000

    #6622 - Error configuración clave ssh Gerrit
    Eliminar la línea de configuración para la clave ssh pública de gerritadmin.

--- scstack/lib/puppet/parser/functions/retrieve_pub_key.rb
+++ scstack/lib/puppet/parser/functions/retrieve_pub_key.rb
@@ -1,9 +1,23 @@
+begin
+  require 'sshkey'
+rescue LoadError
+  puts "sshkey was not found, waiting puppet to install"
+end
+
 # Returns content of ssh key pub file
+# function_retrive_pub_key(["/opt/ssh-keys/gerritadmin_rsa.pub"])
 module Puppet::Parser::Functions
-  newfunction(:retrive_pub_key, :type => :rvalue, :doc => <<-EOS
+  newfunction(:retrieve_pub_key, :type => :rvalue, :doc => <<-EOS
     Returns content of ssh key pub file.
     EOS
   ) do |args|
-    File.read(args[0]) if File::exists?(args[0])
+
+    # /usr/bin/ssh-keygen -t rsa -C 'gerritadmin@$domain' -N '' -f /opt/ssh-keys
+    puts "Reading: " + lookupvar('gerritpublocation') + " file."
+    if File::exists?(lookupvar('gerritpublocation'))
+      k = SSHKey.new(File.read(lookupvar('gerritpublocation')))
+      puts k.ssh_public_key
+      return k.ssh_public_key
+    end
   end
 end
