15e365b68c2d1eaed061aa2b8f62d0c4f532164f
ricardogarcia <ricardogarfe@gmail.com>
2013-03-22 12:42:59 +0000

    #6680 - Planificar la versión 0.3
    Versión 0.4-SNAPSHOT de SidelabcodeStack para comenzar el desarrollo.

--- es.sidelab.scstack.parent/publish-build.sh
+++ es.sidelab.scstack.parent/publish-build.sh
@@ -4,7 +4,7 @@ username=$1
 password=$2
 lftp -u ${username},${password} sftp://code.sidelab.es <<EOF
 put -O /public/sidelabcodestack/artifacts/0.3 ${WORKSPACE}/es.sidelab.scstack.service/target/scstack-service-bin.tar.gz
-put -O /public/sidelabcodestack/artifacts/0.3 ${WORKSPACE}/es.sidelab.scstack.puppet-installer/target/puppet-installer-0.3-bin.tar.gz
+put -O /public/sidelabcodestack/artifacts/0.3 ${WORKSPACE}/es.sidelab.scstack.puppet-installer/target/puppet-installer-0.4-SNAPSHOT-bin.tar.gz
 bye
 EOF
 
