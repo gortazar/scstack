076753a90ac340898e33c871f32177a04844c6b7
ricardogarcia <ricardogarfe@gmail.com>
2013-04-23 10:27:32 +0000

    6687 - Incluir Redmine Backlogs Plugin en la forja
    Instalación del plugin backlog a través de puppet.
    
    TODO: configuración de los parámetros.

---
+++ puppet_plugins_redmine/manifests/backlogs_plugin.pp
@@ -0,0 +1,80 @@
+# Class: backlogs_plugin
+#
+# This module manages Redmine plugins installation.
+#
+# Parameters: $installFolder: Redmine Installation Directory.
+#
+# Actions:
+#
+#   Install backlogs_plugin into existent redmine installation.
+#
+# Requires: Redmine Installation.
+#
+class puppet_plugins_redmine::backlogs_plugin ($installFolder) {
+  Class["scstack::redmine"] -> Class["puppet_plugins_redmine::backlogs_plugin"]
+
+  package { "libxslt-dev": ensure => installed, }
+
+  package { "libxml2-dev":
+    require => Package["libxslt-dev"],
+    ensure  => installed,
+  }
+
+  # Download plugin
+  exec { "download-backlogs":
+    cwd       => "/tmp",
+    command   => "/usr/bin/wget  -c  http://code.sidelab.es/public/sidelabcodestack/artifacts/0.4/redmine_backlogs.tar.gz",
+    logoutput => true,
+    require   => Package["libxml2-dev"],
+  }
+
+  # Unzip plugin
+  exec { "extract-backlogs":
+    cwd     => "/tmp",
+    command => "/bin/tar -xvzf redmine_backlogs.tar.gz",
+    require => Exec["download-backlogs"],
+  }
+
+  exec { "move-backlogs":
+    cwd       => "/tmp",
+    command   => "/bin/mv redmine_backlogs $installFolder/redmine/plugins/redmine_backlogs",
+    require   => Exec["extract-backlogs"],
+    logoutput => true,
+  }
+
+  # Configure Redmine
+  exec { "bundle-install":
+    cwd         => "$installFolder/redmine",
+    logoutput   => true,
+    require     => File["$installFolder/redmine/plugins/redmine_backlogs"],
+    command     => "/usr/local/bin/bundle install --path vendor/bundle --without postgresql development test rmagick sqlite",
+    environment => "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/opt/vagrant_ruby/bin",
+  }
+
+  # Migrate plugin in redmine directory
+  exec { "migrate-plugin-backlogs":
+    cwd         => "$installFolder/redmine",
+    require     => Exec["bundle-install"],
+    environment => ["RAILS_ENV=production"],
+    command     => "/usr/local/bin/bundle exec rake redmine:plugins:migrate",
+    logoutput   => true,
+  }
+
+  # Install backlogs plugin
+  exec { "bundle-install-plugin-backlogs":
+    cwd         => "$installFolder/redmine",
+    require     => Exec["migrate-plugin-backlogs"],
+    logoutput   => true,
+    command     => "/usr/local/bin/bundle exec rake redmine:backlogs:install",
+    environment => "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/opt/vagrant_ruby/bin",
+  }
+
+  # Update users and grop configuration
+  file { "$installFolder/redmine/plugins/redmine_backlogs":
+    owner   => www-data,
+    group   => www-data,
+    recurse => true,
+    require => Exec["bundle-install-plugin-backlogs"],
+  }
+
+}
