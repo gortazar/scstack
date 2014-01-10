d301f330610460a81dc0b4d5b8e519757aebe59c
patxi <patxi.gortazar@gmail.com>
2014-01-10 16:44:26 +0100

    Added exec permissions

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,4 @@
 
 apt-get install wget, puppet
 
-wget https://github.com/sidelab-urjc/scstack/archive/master.zip
-unzip scstack-master.zip
-
-cd scstack-master
 puppet apply --modulepath=`pwd` default.pp
