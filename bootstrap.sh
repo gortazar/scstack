ec62577f8e70598b757271ad8658deed63fb7da5
patxi <patxi.gortazar@gmail.com>
2014-01-19 00:35:02 +0100

    Delegating ruby installation to travis

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ apt-get update
 
 apt-get -y install wget puppet
 
-./install-rvm.sh stable
-./install-ruby.sh 1.9.3
+# ./install-rvm.sh stable
+# ./install-ruby.sh 1.9.3
 
 puppet apply --modulepath=`pwd` default.pp
